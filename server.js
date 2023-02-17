929d0b9fbbdac0014d23ca7130946a7fe687bfca
Nora Mensah <noramens@mpharma.com>
2023-02-17 19:18:38 +0000

    initial author routes

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index');
+const authorsRouter = require('./routes/authors');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL);
@@ -20,5 +23,6 @@ db.on('error', error => console.log(error));
 db.once('open', () => console.log('Connection to mongoose successful'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorsRouter);
 
 app.listen(process.env.PORT || 3000);
